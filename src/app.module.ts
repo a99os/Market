c02dd0e22411b5f4e28953e0fb2800bc1da926e9
a99os <abbosashirboev@gmail.com>
2022-12-10 19:09:21 +0500

    add product crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MarketsModule } from './markets/markets.module';
 import { BranchesModule } from './branches/branches.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [MarketsModule, BranchesModule],
+  imports: [MarketsModule, BranchesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
