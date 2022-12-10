baada5eed2bf2b295aeb3cfa25ea6090cde95b17
a99os <abbosashirboev@gmail.com>
2022-12-10 19:20:42 +0500

    add workers crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MarketsModule } from './markets/markets.module';
 import { BranchesModule } from './branches/branches.module';
 import { ProductsModule } from './products/products.module';
+import { WorkersModule } from './workers/workers.module';
 
 @Module({
-  imports: [MarketsModule, BranchesModule, ProductsModule],
+  imports: [MarketsModule, BranchesModule, ProductsModule, WorkersModule],
   controllers: [AppController],
   providers: [AppService],
 })
