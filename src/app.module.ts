96b9cf2fda764d568322f0320b33c599ca67e8a3
a99os <abbosashirboev@gmail.com>
2022-12-12 14:48:39 +0500

    add module sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { BranchesModule } from './branches/branches.module';
 import { ProductsModule } from './products/products.module';
 import { WorkersModule } from './workers/workers.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'mouse.db.elephantsql.com',
+      port: 3306,
+      username: 'aeykzcag',
+      password: 'ursBuyUseOR1nwE3YI3VNs3xzOI0qClu',
+      database: 'aeykzcag',
+      models: [],
+    }),
+    ,
     ConfigModule.forRoot(),
     MarketsModule,
     BranchesModule,
